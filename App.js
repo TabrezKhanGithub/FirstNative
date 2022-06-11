8053b5b3ec240e99d68f2e786ddb631d5069f8af
TabrezKhanGithub <Tabrezkhan@0689>
2022-06-11 16:08:18 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+export default class App extends Component {
+  render(){
+    return(
+      <View>
+        <Text>Welcome to the React Native</Text>
+        <Text>This is my First App</Text>
+      </View>
+    );
+  }
+}
