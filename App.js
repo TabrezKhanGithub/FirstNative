0f2b0df37abb5ce8762734e8a69042d714494dc9
TabrezKhanGithub Github <tabrezkhan011@gmail.com>
2022-06-19 01:56:27 +0530

    Button with TouchableOpacity and Pressable

--- App.js
+++ App.js
@@ -1,116 +1,75 @@
-import { RefreshControl, ScrollView,FlatList, StyleSheet, Text, View, SectionList } from 'react-native'
-import React,{useState} from 'react'
-
-
-
+import { Button, Pressable, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+import React, { useState } from 'react'
 
 const App = () => {
-  
-  const [Items,setItems] =useState([
-    {name: 'Item 1'},
-    {name: 'Item 2'},
-    {name: 'Item 3'},
-    {name: 'Item 4'},
-    {name: 'Item 5'},
-    {name: 'Item 6'},
-    {name: 'Item 7'},
-    {name: 'Item 8'},
-    {name: 'Item 9'},
-    {name: 'Item 10'},
-    {name: 'Item 11'},
-  
-  ]);
 
-  const [DATA,setDATA] = useState([
-    {
-      title: 'January',
-      data: ['Item 1-1', 'Item 1-2', 'Item 1-3']
-    },
-    {
-      title: 'Fabuary',
-      data: ['Item 2-1', 'Item 2-2', 'Item 2-3']
-    },
-    {
-      title: 'March',
-      data: ['Item 3-1', 'Item 3-2', 'Item 3-3']
-    },
-    {
-      title: 'April',
-      data: ['Item 4-1', 'Item 4-2', 'Item 4-3']
-    },
-    {
-      title: 'May',
-      data: ['Item 5-1', 'Item 5-2', 'Item 5-3']
-    },
-  ])
-
-  const [Refreshing,setRefreshing] = useState(false);
-  const onRefresh = () => {
-    setRefreshing(true);
-    setDATA([...DATA,   {
-      title: 'June',
-      data: ['Item 5-1', 'Item 5-2', 'Item 5-3']
-    }]);
-    setRefreshing(false);
-  }  
+  const [name,SetName] =useState('');
+  const[submitted,SetSubmitted] = useState(false);
+  const [inputState,setInputState] = useState(name);
+  const onClickHandler = () => {
+    SetSubmitted(!submitted);
+  }
   return (
-      <SectionList
-        keyExtractor={(item, index) => index.toString()}
-        sections = {DATA}
-        renderItem = {({item}) => (
-          <Text style = {styles.text}>{item}</Text>
-
-        )}
-        renderSectionHeader = {({section}) => (
-          <View style = {styles.item}>
-             <Text style = {styles.text}>{section.title}</Text>
-          </View>
-        )}
-        refreshControl ={<RefreshControl refreshing = {Refreshing} onRefresh= {onRefresh} colors ={['#f538d5']}/> }
+    <View style ={styles.body}>
+      <Text style ={styles.text}>Please Write your name: </Text>
+      <TextInput placeholder='e.g. John' style={styles.input} 
+      onChangeText ={(value) => SetName(value)}
       />
-      // <FlatList
-      //   keyExtractor={(item,index) => index.toString()}
-      //   data={Items}
-      //   renderItem ={({item}) => (
-      //       <View style = {styles.item}>
-      //             <Text style = {styles.text}>{item.name}</Text>
-      //       </View>
-      //   )}
-      //   refreshControl ={<RefreshControl refreshing = {Refreshing} onRefresh= {onRefresh} colors ={['#f538d5']}/> }
-      // />
-      // <ScrollView style = {styles.body}
-      //   refreshControl ={<RefreshControl refreshing = {Refreshing} onRefresh= {onRefresh} colors ={['#f538d5']}/> }
-      // >
-      //   {
-      //     Items.map((object) => {
-      //       return (
-      //         <View style = {styles.item} key = {object.key}>
-      //           <Text style = {styles.text}>{object.name}</Text>
-      //         </View> 
-      //       )
-      //     })
-      //   }
-      // </ScrollView>   
-      
+      <Pressable
+      onPress={onClickHandler}
+      style= {({pressed})=>[
+          {backgroundColor: pressed ? '#dddddd' : '#00ff22'},
+      styles.button    
+      ]}
+      >
+        <Text style ={styles.text}>{submitted ? 'clear':'Submit'}</Text>
+      </Pressable>
+      {/* <Button title= { submitted ? 'clear':'Submit'} onPress={onClickHandler} disabled={submitted}/> */}
+      {/* <TouchableOpacity onPress={onClickHandler}
+      style = {styles.button}
+      activeOpacity={0.6}
+      >
+        <Text style ={styles.text}>{submitted ? 'clear':'Submit'}</Text>
+      </TouchableOpacity> */}
+     {/* ---- <TouchableHighlight/>
+      <TouchableWithoutFeedback/> ----- */}
+      {
+        submitted ? <Text style = {styles.text}>You are registered as {name}</Text> : null
+      }
+    </View>
   )
-
 }
+
 export default App
 
 const styles = StyleSheet.create({
   body :{
-    // flex :1,
-    flexDirection : 'column',
-    backgroundColor: '#ffffff' 
+    flex :1,
+    alignItems :'center'
   },
-  item : {
-    backgroundColor : '#5fdade',
-    justifyContent:'center',
+  text:{
+    fontSize :24
+  },
+  input:{
+    borderWidth :2,
+    // height : 50,
+    width: 140,
+    borderRadius: 5,
+    margin :10,
+    textAlign:'center',
+    fontSize :20,
+  },
+  textinput :{
+    textAlign :'center',
+    fontSize : 20,
+  },
+  button:{
+    margin: 10,
+    width: 150,
+    height :50,
     alignItems: 'center',
-    margin : 12
-  },
-  text : {
-    fontSize : 42,
-    // color : '#ffffff'
+    justifyContent: 'center',
+    borderRadius :10,
+    backgroundColor : '#00ff22'
   }
 })
